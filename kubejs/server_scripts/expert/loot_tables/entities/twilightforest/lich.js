fbccc8f1b0dcc2a6a5df11b21203370859bc4dbf
StarskyXIII <starskyxv@gmail.com>
2024-05-19 23:39:11 +0800

    更新至 v1.21.0

--- kubejs/server_scripts/expert/loot_tables/entities/twilightforest/lich.js
+++ kubejs/server_scripts/expert/loot_tables/entities/twilightforest/lich.js
@@ -4,24 +4,35 @@ ServerEvents.genericLootTables((event) => {
     }
     event.modify('twilightforest:entities/lich', (table) => {
         table.addPool((pool) => {
-            pool.rolls = [3, 5];
+            pool.rolls = 4;
             pool.addItem(
                 Item.of(
                     'ars_nouveau:novice_spell_book',
                     '{color:12,display:{Lore:[\'{"text":"這究竟是什麼物種的皮革呢？","color":"dark_red"}\'],Name:\'{"text":"破舊的法術書","color":"dark_purple"}\'}}'
                 ),
+                1,
                 1
             );
         });
 
         table.addPool((pool) => {
-            pool.rolls = [1, 1];
+            pool.rolls = 1;
             pool.addItem(Item.of('minecraft:enchanted_book').enchant('spirit:soul_reaper', 1), 1, 1);
         });
 
         table.addPool((pool) => {
-            pool.rolls = [5, 15];
-            pool.addItem('twilightforest:charm_of_life_2', 1, 1);
+            pool.rolls = 1;
+            pool.addItem('twilightforest:charm_of_life_2', 1, [5, 15]);
+        });
+
+        table.addPool((pool) => {
+            pool.rolls = 1;
+            pool.addItem(Item.of('ars_elementa:anima_essence'), 1, [10, 16]);
+        });
+
+        table.addPool((pool) => {
+            pool.rolls = 1;
+            pool.addItem(Item.of('ars_nouveau:abjuration_essence'), 1, [10, 16]);
         });
     });
 });
